696d28df87384a2df4b9fc744b1591caac441c8b
leftskidlow <32857018+leftskidlow@users.noreply.github.com>
2022-03-20 11:08:17 -0400

    Exer 3 Final

---
+++ BinarySearchTree.playground/Pages/Exercise 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,103 @@
+class BinarySearchTree<T: Comparable & CustomStringConvertible> {
+    
+    /* Instance Variables */
+    private var root: BinaryNode<T>?
+    
+    /* Public Functions */
+    
+    // add(_:) -> Users add values to BST, if there is no root, it gets added here
+    
+    
+    
+    
+    /* Private Functions */
+    
+    /* add(_:to:)
+     *  Recurses (preorder) down the tree to find where to put the new value
+     *  and is does not allow duplicate values */
+    private func add(_ node: BinaryNode<T>, to parent: BinaryNode<T>) {
+        if node.data < parent.data {
+            if let existingLeftNode = parent.leftChild {
+                add(node, to: existingLeftNode)
+            } else {
+                parent.leftChild = node
+            }
+        } else if node.data > parent.data {
+            if let existingRightNode = parent.rightChild {
+                add(node, to: existingRightNode)
+            } else {
+                parent.rightChild = node
+            }
+        }
+    }
+    
+}
+
+class BinaryNode<T> {
+    
+    /* Instance Variables */
+    var data: T
+    var leftChild: BinaryNode?
+    var rightChild: BinaryNode?
+    
+    /* Initializers */
+    init(data: T) {
+        self.data = data
+    }
+}
+
+/* Final Save Point
+ 
+class BinarySearchTree<T: Comparable & CustomStringConvertible> {
+    
+    /* Instance Variables */
+    private var root: BinaryNode<T>?
+    
+    /* Public Functions */
+    
+    // add(_:) -> Users add values to BST, if there is no root, it gets added here
+    func add(_ value: T) {
+        let node = BinaryNode(data: value)
+        if let root = root {
+            add(node, to: root)
+        } else {
+            self.root = node
+        }
+    }
+    
+    /* Private Functions */
+    
+    /* add(_:to:)
+     *  Recurses (preorder) down the tree to find where to put the new value
+     *  and is does not allow duplicate values */
+    private func add(_ node: BinaryNode<T>, to parent: BinaryNode<T>) {
+        if node.data < parent.data {
+            if let existingLeftNode = parent.leftChild {
+                add(node, to: existingLeftNode)
+            } else {
+                parent.leftChild = node
+            }
+        } else if node.data > parent.data {
+            if let existingRightNode = parent.rightChild {
+                add(node, to: existingRightNode)
+            } else {
+                parent.rightChild = node
+            }
+        }
+    }
+    
+}
+
+class BinaryNode<T> {
+    
+    /* Instance Variables */
+    var data: T
+    var leftChild: BinaryNode?
+    var rightChild: BinaryNode?
+    
+    /* Initializers */
+    init(data: T) {
+        self.data = data
+    }
+}
+ */
